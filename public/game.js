40acc508059ddee56b33ec73d26c30a6c62765e1
maithili <maithiliw27@gmail.com>
2025-09-22 18:05:57 +0530

    added html canvas and basic frontend rendering

--- public/game.js
+++ public/game.js
@@ -2,44 +2,100 @@
 const socket = io(); 
 console.log('connecting..')
 
-          socket.on("connect",()=>{
-          console.log(`conected to server`,socket.id);
-          });
+const canvas=document.querySelector(".gameBoard");
+const ctx=canvas.getContext("2d");
 
-const inputRight=document.querySelector(".right");
-const inputLeft=document.querySelector(".left");
-const inputAhead=document.querySelector(".ahead");
-const inputAccelerate=document.querySelector(".accelerate");
+socket.on("connect",()=>{
+     console.log(`conected to server`,socket.id); 
+     });
 
-let clientInput={
-     id:socket.id,
-     action:"move",
-};
+     const Track=[
+          {x:170, y:0, width:300, height:100},
+          {x:170,y:100,width:300,height:100},
+          {x:170, y:200, width:300, height:100},
+          {x:170,y:300,width:300,height:100},
+          {x:170, y:400, width:300, height:100},
+     ];
+
+     Track.forEach(trackPiece => {
+          ctx.fillStyle="black";
+          ctx.fillRect(trackPiece.x , trackPiece.y ,trackPiece.width,trackPiece.height);
+         
+          
+     });
 
 document.addEventListener("keydown",(event)=>{
      if(event.key==="ArrowRight"){
-     clientInput.action="turned right";
-     socket.emit("details",clientInput);
+     socket.emit("details",{id:socket.id,action:"turned right"});
      }
      else if(event.key==="ArrowLeft"){
-     clientInput.action= "turned left";
-     console.log(event.key);
-     socket.emit("details",clientInput);
+     socket.emit("details",{id:socket.id,action:"turned left"});
      }
      else if (event.key==="ArrowUp"){
-     clientInput.action= "moved ahead";
-     socket.emit("details",clientInput);
+     socket.emit("details",{id:socket.id,action:"moved ahead"});
      }
      else if(event.key==="a"){
-     clientInput.action= "accelerated";
-     console.log(event.key)
-     socket.emit("details",clientInput);
+     socket.emit("details",{id:socket.id,action:"accelerated"});
      }
 });
 
-  
+     socket.on("playerId",(players)=>{
+           playersId=players;
 
+     });
+     let playersId={};
 
-socket.on("gameUpdate",(gameWorld)=>{
+     socket.on("gameUpdate",(gameWorld)=>{
      console.log(`broadcasted:`,JSON.stringify(gameWorld));
+
+     const myId=playersId[socket.id];
+     const myData=gameWorld[myId];
+     let cameraX=myData.x - canvas.width /2;
+     let cameraY= myData.y - canvas.height/2;
+
+     let margin=10;
+     
+     ctx.clearRect(0,0,canvas.width,canvas.height);
+
+     const Track=[
+          {x:170, y:0, width:300, height:100},
+          {x:170,y:100,width:300,height:100},
+          {x:170, y:200, width:300, height:100},
+          {x:170,y:300,width:300,height:100},
+          {x:170, y:400, width:300, height:100},
+          {x:170,y:500,width:300,height:100}
+     ]
+     Track.forEach(trackPiece => {
+          ctx.fillStyle="black";
+          ctx.fillRect(trackPiece.x - cameraX, trackPiece.y - cameraY,trackPiece.width,trackPiece.height);
+          
+          
+     });
+
+
+  
+     for (const player in gameWorld) {
+     
+          const playerData=gameWorld[player];
+          let playerX=playerData.x - cameraX;
+          let playerY=playerData.y-cameraY;
+          if(player===myId){
+               ctx.fillStyle="blue";
+               ctx.fillRect(canvas.width/2,canvas.height/2,100,80);
+               ctx.fillText(myId,canvas.width/2,canvas.height/2 - margin)
+              
+              
+          }
+          else{
+               ctx.fillStyle="red";
+               ctx.fillRect(playerX,playerY,100,80)
+               ctx.fillText(player,playerX,playerY-margin)
+              
+              
+          }
+     }
+
 });
+
+
+
